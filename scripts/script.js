2bda58429da74d33abf5fe9f19597d06ce0617fb
Lakshya Choudhary <lakshyachoudhary1@gmail.com>
2024-10-09 17:41:51 +0530

    Animations Added, Navbar Updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-const downloadFile = function (filePath, fileName) {
+// File Downloader
+const downloadBrochure = function (filePath, fileName) {
 	// const filePath = "../utils/files/Test.pdf";
 	// const fileName = "Demo.pdf";
 
@@ -16,3 +17,50 @@ const downloadFile = function (filePath, fileName) {
 		}, 100);
 	});
 };
+
+// Swipe In Animation
+function addScrollAnimation() {
+	const observer = new IntersectionObserver(
+		(entries, observer) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add("in-view");
+					observer.unobserve(entry.target);
+				}
+			});
+		},
+		{
+			threshold: 0,
+		}
+	);
+
+	const elementsToAnimate = document.querySelectorAll(
+		".animLeft, .animRight"
+	);
+
+	elementsToAnimate.forEach((element) => {
+		observer.observe(element);
+	});
+}
+addScrollAnimation();
+
+// Navlink Highlight
+const navLinks = document.querySelectorAll(".links");
+
+navLinks.forEach((navLink) => {
+	navLink.addEventListener("mouseover", function () {
+		navLinks.forEach((link) => {
+			if (link !== navLink) {
+				link.style.opacity = "0.5";
+			} else {
+				link.style.opacity = "1";
+			}
+		});
+	});
+
+	navLink.addEventListener("mouseout", function () {
+		navLinks.forEach((link) => {
+			link.style.opacity = "1";
+		});
+	});
+});
