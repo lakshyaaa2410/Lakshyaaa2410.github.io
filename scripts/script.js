cad6084655a38060fa6fca4b022ebb016e3bef13
Lakshya Choudhary <lakshyachoudhary1@gmail.com>
2024-10-15 09:52:15 +0530

    New Pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,37 @@
+const downloadButtonClasses = [
+	"Connector1",
+	"Connector5",
+	"Connector6",
+	"Single_AC",
+	"Dual_AC",
+	"Single_DC",
+	"Dual_DC",
+];
+
+downloadButtonClasses.forEach((btnClass) => {
+	const downloadBtns = document.querySelectorAll(`.${btnClass}`);
+
+	const filePath = `../../utils/files/${btnClass}.pdf`;
+	downloadBtns.forEach((downloadBtn) => {
+		downloadBtn.addEventListener("click", function (event) {
+			const fileName = `${event.target.classList[4]}_${event.target.classList[2]}.pdf`;
+			console.log(fileName);
+
+			downloadBrochure(filePath, fileName);
+		});
+	});
+});
+
 // File Downloader
 const downloadBrochure = function (filePath, fileName) {
-	// const filePath = "../utils/files/Test.pdf";
-	// const fileName = "Demo.pdf";
+	const link = document.createElement("a");
+	link.style.display = "none";
+	link.href = filePath;
+	link.download = fileName;
+	document.body.appendChild(link);
 
-	downloadBtn.addEventListener("click", () => {
-		const link = document.createElement("a");
-		link.style.display = "none";
-		document.body.appendChild(link);
-
-		link.href = filePath;
-		link.download = fileName;
-
-		setTimeout(() => {
-			link.click();
-			document.body.removeChild(link);
-		}, 100);
-	});
+	link.click();
+	document.body.removeChild(link);
 };
 
 // Swipe In Animation
@@ -62,5 +78,27 @@ navLinks.forEach((navLink) => {
 		navLinks.forEach((link) => {
 			link.style.opacity = "1";
 		});
+	});
+});
+
+// Scrolling Popup Test
+
+document.addEventListener("scroll", function () {
+	const sections = document.querySelectorAll("section");
+	let currSection = "";
+
+	sections.forEach((section) => {
+		const sectionTop = section.offsetTop;
+		const sectionHeight = section.clientHeight;
+
+		if (window.scrollY >= sectionTop - sectionHeight / 3)
+			currSection = section.getAttribute("id");
+	});
+
+	navLinks.forEach((link) => {
+		link.classList.remove("active");
+
+		if (link.getAttribute("href") === `${currSection}`)
+			link.classList.add("active");
 	});
 });
